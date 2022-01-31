5d91ff6f02f0ddc150ad954b4452a528fdf92a37
Thais <tfrancag@gmail.com>
2022-01-31 15:41:43 -0300

    Remoção de comentários e correção de código

--- src/app.js
+++ src/app.js
@@ -6,52 +6,35 @@ import joi from "joi";
 import dayjs from "dayjs";
 
 const result = dotenv.config();
-//config will read your .env file, parse the contents, assign it to process.env, and return an Object with a parsed key containing the loaded content or an error key if it failed.
 if (result.error) {
-  console.log("Deu Algum Problema");
+  console.log("Houve algum problema");
 }
-
-//esse console.log vai me retornar a informação dentro do .env o que, no caso, se refere à connection String
-
-//tem que lembrar de conectar com o MongoClient
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 mongoClient.connect(() => {
   db = mongoClient.db("projeto12_batePapoUol");
 });
-//Com isso, eu estou criando o meu banco de dados, que é mantido aberto usando o express(). O que está dentro do db é o nome que eu estou dando pro Banco de Dados em questão. Poderia ser qualquer nome, e o no momento que é nomeado ele passa a existir. A princípio, sendo feita a conexào de forma global, não é necessário o fechar, ele é fechado quando é fechada a aplicação, quando o express para de rodar
-
-// db.createCollection('participants');
-// db.createCollection('messages');
-//tecnicamente, não precisa criar as coleções, estou fazendo só pra me ajudar  a pensar
 
 const app = express();
 app.use(express.json());
-//Tem que usar o json para que o express receba informações de uma forma que ele seja capaz de compreender
-
 app.use(cors());
-//O cors, ou Cross-Origin Sharing, permite o acesso a certos hosts. Basicamente, permite que quaisquer orignes além do seu próprio possam carregar e acessar recursos
 
 const participantsSchema = joi.object({
   name: joi.string().required(),
-  // lastStatus: joi.number().required()
 });
 
 const messagesSchema = joi.object({
   to: joi.string().required(),
   text: joi.string().required(),
   type: joi.string().valid("message", "private_message").required(),
-  //o .valid diz quais conteúdos de string serão aceitos
-  // from: joi.string().required()
 });
-//A validação da mensagem não tem que incluir o tempo. Esse é adicopnado antes dele ser enviado para a coleção!
 
 app.get("/participants", async (req, res) => {
   try {
     const participants = await db.collection("participants").find().toArray();
     if (participants.length === 0) {
-      res.send("Ainda não há participantes");
+      res.send("Ainda não há participantes!");
     } else {
       res.send(participants);
     }
@@ -63,16 +46,11 @@ app.get("/participants", async (req, res) => {
 
 app.post("/participants", async (req, res) => {
   const user = req.body;
-
-  //Essa constante estava dentro do const. A vcoloquei para fora porque o post nào parava
   const participants = await db.collection("participants").find().toArray();
-  //Eu transformo a minha co;leçào em uma rraya para poder a manipular "procurar"cosias dentro dela.
 
   try {
-    //Eu vou colocar minha validação dentro de um try/catch para evitar deixar meu código explodir por erros inesperados
     const validation = participantsSchema.validate(user);
 
-    //com isso, estou chamando a validação
     if (validation.error) {
       return res
         .status(422)
@@ -84,16 +62,13 @@ app.post("/participants", async (req, res) => {
     for (let i = 0; i < participants.length; i++) {
       if (participants[i].name === user.name) {
         return res.status(418).send("Nome já em uso");
-        //Lembrete: quando der retunr, retornam também o send de erro para não ficar rodando a requisição pra sempre
       }
     }
-    //Agora tem que adicionar o timeStamp
     let loggedUser = {
       name: user.name,
       lastStatus: Date.now(),
     };
     await db.collection("participants").insertOne(loggedUser);
-    //Antes o await estava dentro de uma constante. Removi. Pelo visto deuc erto, mas não tenho certeza quanto ao porque
     res.sendStatus(200);
   } catch (error) {
     console.log(error);
@@ -105,7 +80,6 @@ app.get("/messages", async (req, res) => {
   let currentUser = req.headers.user;
   let latestMessages;
   let limit;
-  //agora entra a parte da query string com o limite
   const messagesLimit = parseInt(req.query.limit);
 
   if (!messagesLimit) {
@@ -115,7 +89,6 @@ app.get("/messages", async (req, res) => {
   }
 
   try {
-    //eu tenho que encontrar todas as mensagens que sejam para everyone e para a pessoa em questao. Ou eu posso ó pegar todas as que sejam fo typo "privada" e conferir se o direcionamento é para a pessoa current USer
     let messages = await db.collection("messages");
 
     let historyMessages = await messages
@@ -127,7 +100,6 @@ app.get("/messages", async (req, res) => {
         ],
       })
       .toArray();
-    //tem que passar os resultados para array para poder os manipular e conferir? Acho que para os retornar, sim, com certeza. Mas o privado acho que ainda pode mexer mais aintes de ter um resultado
     if (historyMessages.length > limit) {
       latestMessages = historyMessages.slice(-limit);
     } else {
@@ -142,11 +114,8 @@ app.get("/messages", async (req, res) => {
 
 app.post("/messages", async (req, res) => {
   const messageFrom = req.headers.user;
-  //colocar validação para se esse for vazio
   const messageContent = req.body;
   const participants = await db.collection("participants");
-
-  //esse é pra pegar a lista de participantes d euma forma que seja compreensivel para ver se o header está dentro da lista. Contudo, eu quero só pegar a coleção, e não a coleção em formato de array. Eu quero pegar como coleção para poder aplicar o "find" do proprio mongo
 
   let userAuthorization = await participants.findOne({ name: messageFrom });
 
@@ -167,9 +136,7 @@ app.post("/messages", async (req, res) => {
           "Entidade não processável. Ou o formato está incorreto, ou faltou algum dado"
         );
     }
-    //depois de passar pela primeira validação, eu tenho que conferir se o header está dentro da lista de usuários, ou seja, na coleção de participants
 
-    //Agora, antes de inserir a mensagem direto na coleçào, ela precisa ser completada, com o header e com o timestamp.
     let messageTime = dayjs().format("HH:mm:ss");
     let sentMessage = {
       from: messageFrom,
@@ -202,7 +169,6 @@ app.post("/status", async (req, res) => {
     if (currentParticipants[i].name === currentUser) {
       userStatus = true;
       currentId = currentParticipants[i]._id;
-      //com isso eu pego o ID sozinho. Mas não acho que é o jeito ideal
       break;
     }
   }
@@ -213,8 +179,6 @@ app.post("/status", async (req, res) => {
     }
 
     let currentTimeStamp = Date.now();
-    //agora que já se conferiu que o user está na lista online, falta atualizar o timeStamp
-    //Primeiro, eu tenho que recuperar o usuário usando o id.
 
     await documentcurrentParticipants.updateOne(
       {
